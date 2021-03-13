e44982295b8f340f1d7a3ecb41a529ff827d2d6c
niki1729 <nikita.michael.kiselev@gmail.com>
2021-03-13 22:27:59 +0100

    Added Buttons to the simple Calculator Screen yet without any functions.

--- labels.py
+++ labels.py
@@ -1,7 +1,10 @@
 # i18n try this out
 TITLE = "UsefullApp"
 
-NDHomeScreen="Home"
+NDHomeScreen = "Home"
 NDCalculator = "Calculator"
 NDChooseWhatYouWant = "Choose what you want"
 NDConverter = "Converter"
+
+CalculatorDelete = "Del"
+CalculatorClear = "C"
